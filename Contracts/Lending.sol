4030d18f6991b25d756fc2c0527192b55768cd34
vmlopez <vmlopez@slashweb.com.mx>
2023-09-22 23:42:25 -0400

    Creating some data types for the contract

--- Contracts/Lending.sol
+++ Contracts/Lending.sol
@@ -6,13 +6,32 @@ contract Lending {
 
     address private owner;
 
-    struct user {
+    // General struct to define users
+    struct User {
         string worldCoinId;
         string lensHandle;
         address[] wallets;
     }
 
-    mapping (uint => user) users;
+    // General struct to define products
+    struct Product {
+        string lat;
+        string lng;
+        string description;
+        uint value;
+        uint price;
+        string image;
+    }
+
+    // General products mapping
+    mapping (uint => Product) Products;
+    // General users mapping
+    mapping (uint => User) Users;
+
+    // Indexes to see what are the user products
+    mapping (uint => uint[]) UserProducts;
+    // Indexes to find user for specific worldcoin ID
+    mapping (string => uint user) WorldCoinId;
 
     constructor() {
         owner = msg.sender;
